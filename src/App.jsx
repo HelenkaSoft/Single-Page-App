8c533347dde1dab90d739b80a68106b379a9a516
Saydva <kacurikjozef@gmail.com>
2024-07-14 14:29:34 +0200

    added Routes and Route from react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { Route, NavLink, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <p>Hello</p>
+      <header>
+        <nav>
+          <NavLink to="/" className="a">
+            Home
+          </NavLink>
+          <NavLink to="/tunes" className="a">
+            Tunes
+          </NavLink>
+          <NavLink to="/about" className="a">
+            About
+          </NavLink>
+        </nav>
+      </header>
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<h1>HomePage</h1>} exact />
+          <Route path="tunes" element={<h1>Tunes</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+        </Routes>
+      </main>
+      <footer></footer>
     </div>
   );
 }
