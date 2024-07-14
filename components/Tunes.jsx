40a483db8c6dcbd2c9e95e7daad6b0306c0253f1
Saydva <kacurikjozef@gmail.com>
2024-07-14 17:32:25 +0200

    workin o tunes, ready to make new components from

--- components/Tunes.jsx
+++ components/Tunes.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
+
 const Tunes = () => {
+  const [search, setsearch] = useState("");
+
+  const handlesearch = (e) => {
+    setsearch(e.target.value);
+  };
+
+  const [songs, setsongs] = useState([
+    { id: 2, artist: "Samco", name: "dodo" },
+    {
+      id: 1,
+      artist: "Sk made",
+      name: "peter",
+    },
+    {
+      id: 3,
+      artist: "Bengoro",
+      name: "tom",
+    },
+  ]);
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    setsongs([...songs, newSong]);
+  };
+
+  const newSong = {
+    id: Math.max(...songs.map((n) => n.id)) + 1,
+    name: search,
+    artist: search,
+  };
+
+  //
   return (
     <div className="home">
       <h1>Tunes</h1>
-      <p>play wit the sounds</p>
+      <form onSubmit={handlesubmit}>
+        <input type="text" value={search} onChange={handlesearch} />
+      </form>
+      <ul>
+        {songs.map((e) => (
+          <li key={e.id}>{JSON.stringify(e)}</li>
+        ))}
+      </ul>
     </div>
   );
 };
